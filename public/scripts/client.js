243847ef60f01c09890d75b19c24c159dbd1d544
Bita <bita.janzadeh@hotmail.com>
2021-12-16 01:37:50 -0500

    reorganized code in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,85 +4,56 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
+const createTweetElement = function (tweetObject) {
+  const element = `
+        <article class="tweet">
 
-  const createTweetElement = function (tweetObject) {
-    const element = `
-          <article class="tweet">
-
-            <header class="tweet-header">
-              <div class="avatar">
-              <img src=${tweetObject.user.avatars} />
-              </div>
-              <div class="name">
-                ${tweetObject.user.name}
-              </div>
-              <div class="handle">
-              ${tweetObject.user.handle}
-              </div> 
-            </header>
-            <div class="tweet-content">
-              <p>
-                ${tweetObject.content.text}
-              </p>
+          <header class="tweet-header">
+            <div class="avatar">
+            <img src=${tweetObject.user.avatars} />
             </div>
-            
-            <footer class="tweet-footer">
-              <div class="date">
-                ${timeago.format(tweetObject.created_at)}
-              </div>
-              <div class="tweet-icons">
-                <i class="fas fa-flag"></i>
-                <i class="fas fa-retweet"></i>
-                <i class="fas fa-heart"></i>
-              </div>
-              </footer>
-          </article>
-      `;
-      return element;
-  }
-  
-  
+            <div class="name">
+              ${tweetObject.user.name}
+            </div>
+            <div class="handle">
+            ${tweetObject.user.handle}
+            </div> 
+          </header>
+          <div class="tweet-content">
+            <p>
+              ${tweetObject.content.text}
+            </p>
+          </div>
+          
+          <footer class="tweet-footer">
+            <div class="date">
+              ${timeago.format(tweetObject.created_at)}
+            </div>
+            <div class="tweet-icons">
+              <i class="fas fa-flag"></i>
+              <i class="fas fa-retweet"></i>
+              <i class="fas fa-heart"></i>
+            </div>
+            </footer>
+        </article>
+    `;
+    return element;
+}
 
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes value and appends it to the tweets container
+/*   Function that loops through array of tweet object, call createTweetElement on each tweet, 
+  then prepend to '#tweets-container'. */
+
   const renderTweets = function(tweets) {
     //clear all child node of #tweets-container
     $('#tweets-container').empty();
-    
     for (const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
       $('#tweets-container').prepend($tweet);
     }
   }
-/* 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
-  renderTweets(data); */
+
+$(document).ready(function() {
 
   /*Event listener to submit a POST request that sends serialized data to server using AJAX */
   $("#submit-tweet").on("submit", function(event) {
@@ -106,7 +77,7 @@ $(document).ready(function() {
         method: 'POST',
         data: $data,
       })
-      .done( (data) => {
+      .done( () => {
         loadtweets();
       })
       .fail( (err) => {
